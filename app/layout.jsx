5bdbd4aa38c94861a8244591dff9f8130f77c449
smartful <remi_rodrigues@hotmail.com>
2024-02-22 14:03:58 +0100

    created layout homepage and styles

---
+++ app/layout.jsx
@@ -0,0 +1,19 @@
+import "@/assets/styles/globals.css";
+
+export const metadata = {
+  title: "PropertyPulse | Find The Perfect Rental",
+  description: "Find your dream rental property",
+  keywords: "rental, find rental, find properties",
+};
+
+const MainLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>
+        <div>{children}</div>
+      </body>
+    </html>
+  );
+};
+
+export default MainLayout;
