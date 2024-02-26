6fc7a8fd745a1c03e85bc5a906e757c8d9d268ba
smartful <remi_rodrigues@hotmail.com>
2024-02-26 21:58:18 +0100

    creation of User and Property models

---
+++ models/Property.js
@@ -0,0 +1,92 @@
+import { Timestamp } from "mongodb";
+import { Schema, models, model } from "mongoose";
+
+const PropertySchema = new Schema(
+  {
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    location: {
+      street: {
+        type: String,
+      },
+      city: {
+        type: String,
+      },
+      state: {
+        type: String,
+      },
+      zipcode: {
+        type: String,
+      },
+    },
+    beds: {
+      type: Number,
+      required: true,
+    },
+    baths: {
+      type: Number,
+      required: true,
+    },
+    square_feet: {
+      type: Number,
+      required: true,
+    },
+    amenities: [
+      {
+        type: String,
+      },
+    ],
+    rates: {
+      nightly: {
+        type: Number,
+      },
+      weekly: {
+        type: Number,
+      },
+      monthly: {
+        type: Number,
+      },
+    },
+    seller_info: {
+      name: {
+        type: String,
+      },
+      email: {
+        type: String,
+      },
+      phone: {
+        type: String,
+      },
+    },
+    images: [
+      {
+        type: String,
+      },
+    ],
+    isFeatured: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    Timestamp: true,
+  }
+);
+
+const Property = models.property || model("Property", PropertySchema);
+
+export default Property;
