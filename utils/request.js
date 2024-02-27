74c0c6f6a8e8d8447da3e7c36ad3b7d09e0dfb64
smartful <remi_rodrigues@hotmail.com>
2024-02-27 02:50:35 +0100

    start property page

--- utils/request.js
+++ utils/request.js
@@ -18,3 +18,22 @@ export const fetchProperties = async () => {
     return [];
   }
 };
+
+export const fetchProperty = async (id) => {
+  try {
+    // Handle the case where the domain is not available yet
+    if (!apiDomain) {
+      return null;
+    }
+
+    const res = await fetch(`${apiDomain}/properties/${id}`);
+    if (!res.ok) {
+      throw new Error("Failed to fetch data");
+    }
+
+    return res.json();
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
