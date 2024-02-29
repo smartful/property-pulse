1c97d35434bc57b5674a2375d32349d607d4a68a
smartful <remi_rodrigues@hotmail.com>
2024-02-29 18:47:01 +0100

    get session user + save new property on DB

--- app/api/properties/route.js
+++ app/api/properties/route.js
@@ -1,5 +1,6 @@
 import connectDB from "@/config/database";
 import Property from "@/models/Property";
+import { getSessionUser } from "@/utils/getSessionUser";
 
 // GET /api/properties
 export const GET = async () => {
@@ -19,6 +20,16 @@ export const GET = async () => {
 
 export const POST = async (request) => {
   try {
+    await connectDB();
+
+    const sessionUser = await getSessionUser();
+
+    if (!sessionUser || !sessionUser.userId) {
+      return new Response("User ID is required", { status: 401 });
+    }
+
+    const { userId } = sessionUser;
+
     const formData = await request.formData();
 
     // Access all values from amenities and images
@@ -52,14 +63,16 @@ export const POST = async (request) => {
         email: formData.get("seller_info.email"),
         phone: formData.get("seller_info.phone"),
       },
-      images,
+      owner: userId,
+      // images,
     };
 
-    console.log("propertyData : ", propertyData);
+    const newProperty = new Property(propertyData);
+    await newProperty.save();
 
-    return new Response(JSON.stringify({ message: "Success" }), {
-      status: 200,
-    });
+    return Response.redirect(
+      `${process.env.NEXTAUTH_URL}/properties/${newProperty._id}`
+    );
   } catch (error) {
     return new Response("Failed to add property", { status: 500 });
   }
