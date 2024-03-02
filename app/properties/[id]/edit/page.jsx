ce6b5fe347479174efe369697d4874be99e5bde5
smartful <remi_rodrigues@hotmail.com>
2024-03-02 05:49:55 +0100

    Start on feature to edit property

---
+++ app/properties/[id]/edit/page.jsx
@@ -0,0 +1,17 @@
+import PropertyEditForm from "@/components/PropertyEditForm";
+
+const PropertyEditPage = () => {
+  return (
+    <div>
+      <section className="bg-blue-50">
+        <div className="container m-auto max-w-2xl py-24">
+          <div className="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0">
+            <PropertyEditForm />
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default PropertyEditPage;
